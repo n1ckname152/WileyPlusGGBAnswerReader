00d268b9c23611e2b662eb034c6b0198579563db
n1ckname152 <38827730+n1ckname152@users.noreply.github.com>
2020-11-16 00:03:29 -0800

    generalized applet selector

--- background.js
+++ background.js
@@ -18,7 +18,7 @@ function showSolution() {
   var applet = $("[data-param-id]")
   var elementID = applet.attr('id')
 
-  script.textContent = elementID + `.setValue('showsolution', 'true'); `+ elementID + `.setHeight(1000);`;
+  script.textContent = elementID + `.setValue('showsolution', 'true'); `+ elementID + `.setHeight(800);`;
   (document.head || document.documentElement).appendChild(script);
   script.remove();
 }
